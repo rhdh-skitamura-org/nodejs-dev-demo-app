471f7908b39a6b4de6857861d136bf885bc9cad6
skitamura7446 <sk03087446@gmail.com>
2025-06-10 18:38:43 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,21 @@ const router = express.Router()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use((req, res, next) => {
+  const started = process.hrtime.bigint()
+  res.on('finish', () => {
+    const diffMs =
+      Number(process.hrtime.bigint() - started) / 1e6
+    console.log(
+      `${new Date().toISOString()} ` +
+      `${req.ip} ${req.method} ${req.originalUrl} ` +
+      `${res.statusCode} - ${diffMs.toFixed(1)} ms`
+    )
+  })
+  next()
+})
+
+
 router.get('/', function (req, res) {
   res.send('Hello World!!!!!!!!!!')
 })
